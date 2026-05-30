fa50cc07ecd303834e8dd12ebc71ad7990d95d3c
exor4ik <156537430+exor4ik@users.noreply.github.com>
2026-05-30 22:24:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,23 +3,23 @@ const http = require('http');
 const { ExpressPeerServer } = require('peer');
 
 const app = express();
-app.enable('trust proxy');
-
 const server = http.createServer(app);
 
+app.enable('trust proxy');
+
 const peerServer = ExpressPeerServer(server, {
-  path: '/myapp',
   proxied: true,
   debug: true,
 });
 
-app.use(peerServer);
+app.use('/myapp', peerServer);
 
 app.get('/', (req, res) => {
   res.send('PeerJS OK');
 });
 
 const port = process.env.PORT || 10000;
+
 server.listen(port, '0.0.0.0', () => {
   console.log(`Listening on ${port}`);
 });
